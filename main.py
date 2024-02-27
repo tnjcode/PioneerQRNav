c575d003c0ef164f10749fdcb6d322216aa2e1ce
tnjcode <fachrialkayyis@gmail.com>
2024-02-27 11:53:22 +0700

    qrscanner competition code

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import pioneer
 import qrscanner
+import time
 
 pn = pioneer.Pioneer()
-qrscanner.open_window()
+
+while True:
+    qrscanner.open_window()
+    op = qrscanner.qr_detect()
+    print(op)
+
+    if op == "kanan":
+        pn.move_pioneer(3, 0)
+        # time.sleep(1)
+    
+    elif op == "kiri":
+        pn.move_pioneer(0, 3)
+        # time.sleep(1)
+
+    elif op == "mundur":
+        pn.move_pioneer(-5, -5)
+        # time.sleep(1)
+    
+    elif op == "maju":
+        pn.move_pioneer(5,5)
+        # time.sleep(1)
+    
+    else:
+        pn.move_pioneer(0, 0)
